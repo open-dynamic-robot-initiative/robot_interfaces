5b9ddcacabd1141364d835ff1c5ff99357116d2b
manuel <manuel.wuthrich@gmail.com>
2019-04-14 19:24:53 +0200

    redefining Finger, RealFinger and MujocoFinger to have a coherent interface for both the real robot and the simulation. safety checks still need to be moved.

--- include/robot_interfaces/finger.hpp
+++ include/robot_interfaces/finger.hpp
@@ -10,16 +10,68 @@ class Finger
 public:
     typedef Eigen::Vector3d Vector;
 
-    virtual void set_torques(const Vector& desired_torques) = 0;
-    virtual void send_torques() = 0;
+    enum JointIndexing {base, center, tip, joint_count};
 
-    virtual Vector get_sent_torques() const = 0;
+    virtual void constrain_and_apply_torques(const Vector& desired_torques)
+    {
+        constrained_torques_ = constrain_torques(desired_torques);
+        apply_torques(constrained_torques_);
+    }
+
+    virtual Vector get_constrained_torques() const
+    {
+        return constrained_torques_;
+    }
     virtual Vector get_measured_torques() const = 0;
-    virtual Vector get_angles() const = 0;
-    virtual Vector get_angular_velocities() const = 0;
+    virtual Vector get_measured_angles() const = 0;
+    virtual Vector get_measured_velocities() const = 0;
 
-    virtual void pause_motors() = 0;
-    virtual void wait_since_last_send(const double& time_s) = 0;
+    virtual void wait_for_execution() const = 0;
+
+protected:
+    virtual void apply_torques(const Vector& desired_torques) = 0;
+    virtual Vector constrain_torques(const Vector& desired_torques)
+    {
+        /// \todo: the safety checks should go here
+        return desired_torques;
+    }
+
+
+private:
+    Vector constrained_torques_;
 };
+
+
+
+
+template<typename Action, typename Observation> class Robot
+{
+public:
+
+    /**
+     * @brief this function will execute the action. in real-time mode this
+     * means simply that the action will be sent to the system. in
+     * non-real-time mode this means that the action will be simulated for
+     * one time step and then the simulation will be stopped.
+     *
+     * @param action to apply
+     */
+    virtual void constrain_and_apply_action(const Action& action) = 0;
+
+    /**
+     * @brief this function only returns once the current action has been
+     * completely exectued. in real-time mode this means it will wait until
+     * one time step has elapsed since execute has been called.
+     * in non-real-time mode this means that it will wait until the simulator
+     * has ran the action for the lenth of one time step.
+     */
+    virtual void wait_for_execution() const = 0;
+
+    virtual Observation get_observation() const = 0;
+
+};
+
+
+
 
 }
