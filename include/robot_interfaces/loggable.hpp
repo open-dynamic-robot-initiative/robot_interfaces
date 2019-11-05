0c83a82b58098948eb6dd0c7a567bb7100e4a5dc
Shruti Joshi <shrutijoshi98@gmail.com>
2019-11-05 13:48:46 +0100

    implemented robot data structures as loggable objects for generalisation
    templated RobotLogger
    
    checkpoint TODO trailing comma
    
    checkpoint TODO: better implementation of iterator

---
+++ include/robot_interfaces/loggable.hpp
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (C) 2018-2019, Max Planck Gesellschaft
+// Copyright note valid unless otherwise stated in individual files.
+// All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+namespace robot_interfaces
+{
+
+/**
+* @brief Contains definitions of the methods to be implemented by the data types.
+**/
+
+class Loggable
+{
+public:
+
+  /**
+  * @brief Return the names of the fields in the structure.
+  **/
+  virtual std::vector<std::string> get_name() = 0;
+
+  /**
+  * @brief Return the data in the fields of the structure.
+  **/
+  virtual std::vector<std::vector<double>> get_data() = 0;
+};
+
+} //namespace robot_interfaces
