32f639be7331126cb9908d21a74bc01d68c42c90
Shruti Joshi <shrutijoshi98@gmail.com>
2019-11-08 14:46:14 +0100

    implementing methods for reducing redundancy
    status as loggable and templated RobotLogger
    
    generalisation using Loggable objects
    
    comments
    
    cleaning comments

--- include/robot_interfaces/robot_logger.hpp
+++ include/robot_interfaces/robot_logger.hpp
@@ -15,6 +15,7 @@
 
 #include <Eigen/Eigen>
 
+#include <robot_interfaces/infix_iterator.h>
 #include <mpi_cpp_tools/basic_tools.hpp>
 #include <mpi_cpp_tools/dynamical_systems.hpp>
 #include <mpi_cpp_tools/math.hpp>
@@ -28,14 +29,33 @@
 
 namespace robot_interfaces
 {
-
+  /**
+   * @brief To log data from *any* robot (real, simulated, fake).
+   *
+   * The logger logs the timestamp, the time index, and the values of every
+   * observation, action, and status variable. Further loggable data structures
+   * can be added to the logger, which *must* derive from class Loggable.
+   *
+   * @tparam Action
+   * @tparam Observation
+   * @tparam Status
+   */
 template <typename Action, typename Observation, typename Status>
 class RobotLogger
 {
 public:
+    static_assert(std::is_base_of<Loggable, Action>::value,
+                  "Action must derive from Loggable");
+    static_assert(std::is_base_of<Loggable, Observation>::value,
+                  "Observation must derive from Loggable");
+    static_assert(std::is_base_of<Loggable, Status>::value,
+                  "Status must derive from Loggable");
 
-    static_assert(std::is_base_of<Loggable, Action>::value, "Action must derive from Loggable");
-    static_assert(std::is_base_of<Loggable, Observation>::value, "Observation must derive from Loggable");
+    /*
+    * Currently, the level of generalisability of the logger is that we know
+    * the following four structures exist for *any* robot whose data is to be
+    * logged.
+    */
 
     Action applied_action;
     Action desired_action;
@@ -44,6 +64,8 @@ public:
 
     std::shared_ptr<robot_interfaces::RobotData<Action, Observation, Status>>
         logger_data_;
+
+    std::vector<std::string> header_;
 
     int block_size_;
     long int index_;
@@ -54,9 +76,9 @@ public:
     std::string output_file_name_;
 
     RobotLogger(std::shared_ptr<
-                     robot_interfaces::RobotData<Action, Observation, Status>>
-                     robot_data,
-                 int block_size)
+                    robot_interfaces::RobotData<Action, Observation, Status>>
+                    robot_data,
+                int block_size)
         : block_size_(block_size),
           stop_was_called_(false),
           logger_data_(robot_data)
@@ -73,64 +95,171 @@ public:
 
     std::vector<std::string> get_header()
     {
+        std::vector<std::string> observation_name = observation.get_name();
+        std::vector<std::string> applied_action_name =
+            applied_action.get_name();
+        std::vector<std::string> desired_action_name =
+            desired_action.get_name();
+        std::vector<std::string> status_name = status.get_name();
 
-      std::vector<std::string> header;
+        std::vector<std::vector<double>> observation_data =
+            observation.get_data();
+        std::vector<std::vector<double>> applied_action_data =
+            applied_action.get_data();
+        std::vector<std::vector<double>> desired_action_data =
+            desired_action.get_data();
+        std::vector<std::vector<double>> status_data = status.get_data();
 
-      std::vector<std::string> observation_name = observation.get_name();
-      std::vector<std::string> applied_action_name = applied_action.get_name();
-      std::vector<std::string> desired_action_name = desired_action.get_name();
-      // std::vector<std::string> status_name = status.get_name();
+        std::string temp;
+        int i, j;
 
-      std::vector<std::vector<double>> observation_data = observation.get_data();
-      std::vector<std::vector<double>> applied_action_data = applied_action.get_data();
-      std::vector<std::vector<double>> desired_action_data = desired_action.get_data();
-      // std::vector<std::vector<double>> status_data = status.get_data();
+        // using the newest index information in logger_data to extract
+        // information on the fields to be logged
+        applied_action =
+            (*logger_data_->applied_action)[logger_data_->applied_action
+                                                ->newest_timeindex()];
+        desired_action =
+            (*logger_data_->desired_action)[logger_data_->desired_action
+                                                ->newest_timeindex()];
+        observation =
+            (*logger_data_
+                  ->observation)[logger_data_->observation->newest_timeindex()];
+        status =
+            (*logger_data_->status)[logger_data_->status->newest_timeindex()];
 
-      std::string temp;
-      int i, j;
+        header_.push_back("#");
+        header_.push_back("[Timestamp]");
+        header_.push_back("(Time Index)");
 
-      //using the newest index information in logger_data to extract information on the fields to be logged
+        append_name_to_header(status_name, status_data);
+        append_name_to_header(observation_name, observation_data);
+        append_name_to_header(applied_action_name, applied_action_data);
+        append_name_to_header(desired_action_name, desired_action_data);
 
-      applied_action = (*logger_data_->applied_action)[logger_data_->applied_action->newest_timeindex()];
-      desired_action = (*logger_data_->desired_action)[logger_data_->desired_action->newest_timeindex()];
-      observation = (*logger_data_->observation)[logger_data_->observation->newest_timeindex()];
-      status = (*logger_data_->status)[logger_data_->status->newest_timeindex()];
+        return header_;
+    }
 
-      header.push_back("#");
-      header.push_back("[Timestamp]");
-      header.push_back("(Time Index)");
+    void append_name_to_header(std::vector<std::string> field_name,
+                               std::vector<std::vector<double>> field_data)
+    {
+        int i, j;
+        std::string temp;
 
-      for(i = 0; i < observation_name.size(); i++)
-      {
-        for(j = 0; j < observation_data[i].size(); j ++)
+        for (i = 0; i < field_name.size(); i++)
         {
-          temp = observation_name[i] + " " + std::to_string(j);
-          header.push_back(temp);
+          if(field_data[i].size() == 1)
+          {
+            temp = field_name[i];
+            header_.push_back(temp);
+          }
+          else
+          {
+            for (j = 0; j < field_data[i].size(); j++)
+            {
+                temp = field_name[i] + " " + std::to_string(j);
+                header_.push_back(temp);
+            }
+          }
         }
-      }
+    }
 
-      for(i = 0; i < applied_action_name.size(); i++)
-      {
-        for(j = 0; j < applied_action_data[i].size(); j ++)
+    //TODO: Using the infix_iterator results in a blank log file.
+    void append_header_to_file()
+    {
+        output_file_.open(output_file_name_);
+
+        std::ostream_iterator<std::string> mid_iterator_string(output_file_,
+                                                               " , ");
+        std::ostream_iterator<std::string> end_iterator_string(output_file_,
+                                                               "\n");
+
+        header_ = get_header();
+
+        std::copy(header_.begin(), header_.end() - 1, mid_iterator_string);
+        std::copy(header_.end() - 1, header_.end(), end_iterator_string);
+
+        output_file_.close();
+    }
+
+    //TODO: fix the working of the infix_iterator and move the redundant writing
+    //part in a separate method.
+    void append_robot_data_to_file()
+    {
+        std::ostream_iterator<double> mid_iterator_double(output_file_, " , ");
+        std::ostream_iterator<double> end_iterator_double(output_file_, "\n");
+
+        std::vector<std::vector<double>> observation_data =
+            observation.get_data();
+        std::vector<std::vector<double>> applied_action_data =
+            applied_action.get_data();
+        std::vector<std::vector<double>> desired_action_data =
+            desired_action.get_data();
+        std::vector<std::vector<double>> status_data = status.get_data();
+
+        int k;
+
+        for (long int j = index_;
+             j < std::min(index_ + block_size_,
+                          logger_data_->observation->newest_timeindex());
+             j++)
         {
-          temp = applied_action_name[i] + " " + std::to_string(j);
-          header.push_back(temp);
+            try
+            {
+                applied_action = (*logger_data_->applied_action)[j];
+                desired_action = (*logger_data_->desired_action)[j];
+                observation = (*logger_data_->observation)[j];
+                status = (*logger_data_->status)[j];
+
+                status_data = status.get_data();
+                observation_data = observation.get_data();
+                applied_action_data = applied_action.get_data();
+                desired_action_data = desired_action.get_data();
+
+                output_file_ << logger_data_->observation->timestamp_s(j)
+                             << " , " << j << " , ";
+
+                for (k = 0; k < status_data.size(); k++)
+                {
+                    std::copy(status_data[k].begin(),
+                              status_data[k].end(),
+                              mid_iterator_double);
+                }
+
+                for (k = 0; k < observation_data.size(); k++)
+                {
+                    std::copy(observation_data[k].begin(),
+                              observation_data[k].end(),
+                              mid_iterator_double);
+                }
+
+                for (k = 0; k < applied_action_data.size(); k++)
+                {
+                    std::copy(applied_action_data[k].begin(),
+                              applied_action_data[k].end(),
+                              mid_iterator_double);
+                }
+
+                for (k = 0; k < desired_action_data.size(); k++)
+                {
+                    std::copy(desired_action_data[k].begin(),
+                              desired_action_data[k].end(),
+                              mid_iterator_double);
+                }
+
+                std::copy(
+                    desired_action_data[desired_action_data.size() - 1].end() -
+                        1,
+                    desired_action_data[desired_action_data.size() - 1].end(),
+                    end_iterator_double);
+            }
+
+            catch (const std::exception &e)
+            {
+                std::cout << "Trying to access index older than the "
+                             "oldest! Skipping ahead."
+                          << std::endl;
+            }
         }
-      }
-
-      for(i = 0; i < desired_action_name.size(); i++)
-      {
-        for(j = 0; j < desired_action_data[i].size(); j ++)
-        {
-          temp = desired_action_name[i] + " " + std::to_string(j);
-          header.push_back(temp);
-        }
-      }
-
-      header.push_back("Status");
-
-      return header;
-
     }
 
     static void *write(void *instance_pointer)
@@ -141,22 +270,9 @@ public:
 
     void write()
     {
+        append_header_to_file();
 
-        std::vector<std::vector<double>> observation_data = observation.get_data();
-        std::vector<std::vector<double>> applied_action_data = applied_action.get_data();
-        std::vector<std::vector<double>> desired_action_data = desired_action.get_data();
-
-        output_file_.open(output_file_name_);
-        std::ostream_iterator<std::string> mid_iterator(output_file_, " , ");
-        std::ostream_iterator<std::string> end_iterator(output_file_, "\n");
-        std::vector<std::string> header = get_header();
-        std::copy(header.begin(), header.end() - 1, mid_iterator);
-        std::copy(header.end() - 1, header.end(), end_iterator);
-
-        output_file_ << std::endl;
-        output_file_.close();
-
-        int k;
+        output_file_.open(output_file_name_, std::ios_base::app);
 
         while (!stop_was_called_ &&
                !(logger_data_->desired_action->length() > 0))
@@ -171,38 +287,11 @@ public:
             if (index_ + block_size_ <=
                 logger_data_->observation->newest_timeindex())
             {
-                output_file_.open(output_file_name_, std::ios_base::app);
-
-
 #ifdef VERBOSE
                 auto t1 = std::chrono::high_resolution_clock::now();
 #endif
 
-                for (long int j = index_; j < index_ + block_size_; j++)
-                {
-                    try
-                    {
-                        std::ostream_iterator<double> mid_iterator_(output_file_, " , ");
-                        std::ostream_iterator<double> end_iterator_(output_file_, "\n");
-                        applied_action = (*logger_data_->applied_action)[j];
-                        desired_action = (*logger_data_->desired_action)[j];
-                        observation = (*logger_data_->observation)[j];
-                        status = (*logger_data_->status)[j];
-
-                        observation_data = observation.get_data();
-                        applied_action_data = applied_action.get_data();
-                        desired_action_data = desired_action.get_data();
-
-                        output_file_ << logger_data_->observation->timestamp_s(j) << " , " << j << " , " ;
-
-                        for(k = 0; k < observation_data.size(); k++)
-                        {
-                        std::copy(observation_data[k].begin(), observation_data[k].end(), mid_iterator_);
-                        }
-
-                        for(k = 0; k < applied_action_data.size(); k++)
-                        {
-                          std::copy(applied_action_data[k].begin(), applied_action_data[k].end(), mid_iterator_);
+                append_robot_data_to_file();
 
                         }
 
@@ -230,11 +319,11 @@ public:
 // to check whether the data being requested to be logged is in the buffer of
 // the timeseries and inspect effect of delays
 #ifdef VERBOSE
-                    std::cout << "Index trying to access, oldest index in the "
-                                 "buffer: "
-                              << j << ","
-                              << logger_data_->observation->oldest_timeindex()
-                              << std::endl;
+                std::cout << "Index trying to access, oldest index in the "
+                             "buffer: "
+                          << j << ","
+                          << logger_data_->observation->oldest_timeindex()
+                          << std::endl;
 #endif
 
 // to print the time taken for one block of data to be logged.
@@ -248,11 +337,9 @@ public:
                 std::cout << "Time taken for one block of data to be logged: "
                           << duration << std::endl;
 #endif
-
             }
         }
     }
-
 
     void start(std::string filename)
     {
@@ -262,52 +349,15 @@ public:
 
     void stop()
     {
+        output_file_.open(output_file_name_, std::ios_base::app);
 
-      std::vector<std::vector<double>> observation_data;
-      std::vector<std::vector<double>> applied_action_data;
-      std::vector<std::vector<double>> desired_action_data;
+        append_robot_data_to_file();
 
-      output_file_.open(output_file_name_, std::ios_base::app);
-      std::ostream_iterator<double> mid_iterator_(output_file_, " , ");
-      std::ostream_iterator<double> end_iterator_(output_file_, "\n");
-
-      int k;
-
-      for(int j = index_; j < logger_data_->observation->newest_timeindex(); j++)
-      {
-        applied_action = (*logger_data_->applied_action)[j];
-        desired_action = (*logger_data_->desired_action)[j];
-        observation = (*logger_data_->observation)[j];
-        status = (*logger_data_->status)[j];
-
-        observation_data = observation.get_data();
-        applied_action_data = applied_action.get_data();
-        desired_action_data = desired_action.get_data();
-
-        for(k = 0; k < observation_data.size(); k++)
-        {
-          std::copy(observation_data[k].begin(), observation_data[k].end(), mid_iterator_);
-        }
-
-        for(k = 0; k < applied_action_data.size(); k++)
-        {
-          std::copy(applied_action_data[k].begin(), applied_action_data[k].end(), mid_iterator_);
-
-        }
-
-        for(k = 0; k < desired_action_data.size(); k++)
-        {
-          std::copy(desired_action_data[k].begin(), desired_action_data[k].end() - 1, mid_iterator_);
-          std::copy(desired_action_data[k].end() - 1, desired_action_data[k].end(), end_iterator_);
-        }
-
-      }
-
-      output_file_.close();
+        output_file_.close();
     }
 
 private:
     std::shared_ptr<real_time_tools::RealTimeThread> thread_;
 };
 
-}  // namespace robot_interfaces
+}// namespace robot_interfaces
