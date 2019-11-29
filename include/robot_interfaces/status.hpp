c192de2dda7463cb0fa5a7a5b21d5256cdcf8392
Vincent Berenz <vberenz@tuebingen.mpg.de>
2019-11-29 13:14:44 +0100

    status now standalone

---
+++ include/robot_interfaces/status.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <robot_interfaces/loggable.hpp>
+
+namespace robot_interfaces
+{
+
+  struct Status : public Loggable
+  {
+    
+    uint32_t action_repetitions = 0;
+    
+    std::vector<std::string> get_name() override
+    {
+      return {"Action_repetitions"};
+    }
+    
+    std::vector<std::vector<double>> get_data() override
+    {
+      return {{action_repetitions}};
+    }
+    
+  };
+
+}
