fdeb09fbbc2ad096d874d4a8069388d48a52fd7e
Vincent Berenz <vberenz@tuebingen.mpg.de>
2019-12-09 15:57:29 +0100

    added copyright head to status, fixed indentation

--- include/robot_interfaces/status.hpp
+++ include/robot_interfaces/status.hpp
@@ -1,3 +1,11 @@
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (C) 2018-2019, Max Planck Gesellschaft
+// Copyright note valid unless otherwise stated in individual files.
+// All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
 #pragma once
 
 #include <string>
@@ -7,21 +15,21 @@
 namespace robot_interfaces
 {
 
-  struct Status : public Loggable
-  {
-    
-    uint32_t action_repetitions = 0;
-    
-    std::vector<std::string> get_name() override
+    struct Status : public Loggable
     {
-      return {"Action_repetitions"};
-    }
     
-    std::vector<std::vector<double>> get_data() override
-    {
-      return {{static_cast<double>(action_repetitions)}};
-    }
+        uint32_t action_repetitions = 0;
     
-  };
+        std::vector<std::string> get_name() override
+        {
+            return {"Action_repetitions"};
+        }
+    
+        std::vector<std::vector<double>> get_data() override
+        {
+            return {{static_cast<double>(action_repetitions)}};
+        }
+    
+    };
 
 }
