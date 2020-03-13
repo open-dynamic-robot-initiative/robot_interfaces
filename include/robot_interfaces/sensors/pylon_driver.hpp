c576dde7b8fc6890a648fc4edfd85020c9d8d237
Shruti Joshi <shrutijoshi98@gmail.com>
2020-03-13 19:47:48 +0100

    clean driver

--- include/robot_interfaces/sensors/pylon_driver.hpp
+++ include/robot_interfaces/sensors/pylon_driver.hpp
@@ -1,85 +1,90 @@
+/**
+ * @file
+ * @brief Driver to interface with the camera using Pylon abd OpenCV.
+ * @copyright 2020, New York University, Max Planck Gesellschaft. All rights
+ *            reserved.
+ * @license BSD 3-clause
+ */
+
 #pragma once
 
-#include <iostream>
 #include <ctime>
+#include <iostream>
 
-#include <real_time_tools/process_manager.hpp>
-#include <real_time_tools/thread.hpp>
-#include <real_time_tools/threadsafe/threadsafe_timeseries.hpp>
 #include <real_time_tools/timer.hpp>
 
 #include <robot_interfaces/sensors/camera_observation.hpp>
 #include <robot_interfaces/sensors/sensor_driver.hpp>
 
-#include <opencv2/opencv.hpp>
 #include <pylon/PylonIncludes.h>
+#include <opencv2/opencv.hpp>
 
 namespace robot_interfaces
 {
-
+/**
+ * @brief Driver for interacting with a camera via Pylon and store
+ * images using OpenCV.
+ */
 class PylonDriver : public SensorDriver<CameraObservation>
 {
 public:
+    Pylon::PylonAutoInitTerm auto_init_term_;
+    Pylon::CInstantCamera camera_;
+    Pylon::CImageFormatConverter format_converter_;
+    Pylon::CPylonImage pylon_image_;
 
-    Pylon::PylonAutoInitTerm auto_init_term;
-    GenApi::CIntegerPtr width, height;
-    
-    Pylon::CImageFormatConverter format_converter;
-    Pylon::CPylonImage pylon_image;
-    Pylon::CGrabResultPtr ptr_grab_result;
-    
-    Pylon::CInstantCamera camera;
-
-    cv::VideoCapture video_capture;
-    real_time_tools::Timer timer;
-    static const uint32_t count_of_images_to_grab = 1;
-
-    PylonDriver() : camera(Pylon::CTlFactory::GetInstance().CreateFirstDevice())
-    { 
-      GenApi::INodeMap& nodemap = camera.GetNodeMap();
-      camera.Open();
-      width = nodemap.GetNode("Width");
-      height = nodemap.GetNode("Height");
-      
-      camera.MaxNumBuffer = 5;
-
-      format_converter.OutputPixelFormat = Pylon::PixelType_BGR8packed;  
+    PylonDriver()
+        : camera_(Pylon::CTlFactory::GetInstance().CreateFirstDevice())
+    {
+        camera_.Open();
+        camera_.MaxNumBuffer = 5;
+        format_converter_.OutputPixelFormat = Pylon::PixelType_BGR8packed;
     }
 
+    /**
+     * @brief Dummy method (Pylon raises exceptions elsewhere in case access
+     * is not successful).
+     * @return true
+     */
     bool is_access_successful()
     {
-      return true;
-      // try
-      // {
-      //   pylon_init();
-      // }
-      // catch(const std::exception& e)
-      // {
-      //   std::cerr << e.what() << '\n';
-      // }
-      
+        return true;
     }
 
+    /**
+     * @brief Get the latest observation (image frame + timestamp of when the
+     * frame's grabbed).
+     * @return CameraObservation
+     */
     CameraObservation get_observation()
-    {          
+    {
         CameraObservation image_frame;
-        cv::Mat frame;
-        double current_time = timer.get_current_time_sec();
-        camera.StartGrabbing();
+        Pylon::CGrabResultPtr ptr_grab_result;
 
-        while(camera.IsGrabbing())
+        try
         {
-          camera.RetrieveResult(15000, ptr_grab_result, Pylon::TimeoutHandling_ThrowException);
-          image_frame.time_stamp = timer.get_current_time_sec();
-          if(ptr_grab_result->GrabSucceeded())
-          {
-            format_converter.Convert(pylon_image, ptr_grab_result);
-            image_frame.image = cv::Mat(ptr_grab_result->GetHeight(), ptr_grab_result->GetWidth(),
-                                        CV_8UC3, (uint8_t*)pylon_image.GetBuffer());
-          }
+            camera_.StartGrabbing();
+            camera_.RetrieveResult(
+                5000, ptr_grab_result, Pylon::TimeoutHandling_ThrowException);
+            image_frame.time_stamp =
+                real_time_tools::Timer::get_current_time_sec();
+            if (ptr_grab_result->GrabSucceeded())
+            {
+                format_converter_.Convert(pylon_image_, ptr_grab_result);
+                image_frame.image = cv::Mat(ptr_grab_result->GetHeight(),
+                                            ptr_grab_result->GetWidth(),
+                                            CV_8UC3,
+                                            (uint8_t*)pylon_image_.GetBuffer());
+            }
+            camera_.StopGrabbing();
         }
+        catch (const std::exception& e)
+        {
+            std::cerr << "Failed to access images from the camera."
+                      << std::endl;
+        }
+        return image_frame;
     }
-
 };
 
-} //namespace robot_interfaces
+}  // namespace robot_interfaces
