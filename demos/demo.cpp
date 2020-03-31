7ac08928e6da6158f018903c51be3674cb0f9b0f
Vincent Berenz <vberenz@tuebingen.mpg.de>
2020-03-31 16:46:28 +0200

    applied clang_format on demo.cpp and robot.hpp

--- demos/demo.cpp
+++ demos/demo.cpp
@@ -3,46 +3,38 @@
  * @author Vincent Berenz
  * license License BSD-3-Clause
  * @copyright Copyright (c) 2019, Max Planck Gesellschaft.
- * 
+ *
  * @brief Minimal demo of robot driver, backend and frontend
  */
 
-#include "robot_interfaces/robot_driver.hpp"
+#include "robot_interfaces/robot.hpp"
 #include "robot_interfaces/robot_backend.hpp"
+#include "robot_interfaces/robot_driver.hpp"
 #include "robot_interfaces/robot_frontend.hpp"
 #include "robot_interfaces/status.hpp"
-#include "robot_interfaces/robot.hpp"
 
 #include <memory>
 
 /**
  * \example demo.cpp
- * This demo shows robot_interfaces of a 
+ * This demo shows robot_interfaces of a
  * dummy "2dof" robot, in which a dof "position"
  * is represented by an integer
  */
-
-
 
 // Actions to be performed by robot, will be received by Driver
 // An action simply encapsulate two desired position value,
 // one for each dof
 class Action
 {
-  
 public:
-  
-  int values[2];
+    int values[2];
 
-  void print(bool backline)
-  {
-    std::cout << "action: "
-	      << values[0] << " "
-	      << values[1] << " ";
-    if(backline)
-      std::cout << "\n";
-  }
-  
+    void print(bool backline)
+    {
+        std::cout << "action: " << values[0] << " " << values[1] << " ";
+        if (backline) std::cout << "\n";
+    }
 };
 
 // Read from the robot by Driver
@@ -50,184 +42,170 @@ public:
 // for each dof
 class Observation
 {
-  
 public:
-  
-  int values[2];
+    int values[2];
 
-  void print(bool backline)
-  {
-    std::cout << "observation: "
-	      << values[0] << " "
-	      << values[1] << " ";
-    if(backline)
-      std::cout << "\n";
-  }
-  
+    void print(bool backline)
+    {
+        std::cout << "observation: " << values[0] << " " << values[1] << " ";
+        if (backline) std::cout << "\n";
+    }
 };
-
 
 // Send command to the robot and read observation from the robot
 // The dof positions simply becomes the ones set by the latest action,
 // capped between a min and a max value (0 and 1000)
-class Driver : public robot_interfaces::RobotDriver< Action,Observation>
+class Driver : public robot_interfaces::RobotDriver<Action, Observation>
 {
-  
 public:
+    Driver(int min, int max) : min_(min), max_(max)
+    {
+    }
 
-  Driver(int min, int max)
-    : min_(min),max_(max){}
+    // at init dof are at min value
+    void initialize()
+    {
+        state_[0] = min_;
+        state_[1] = min_;
+    }
 
-  // at init dof are at min value
-  void initialize()
-  {
-    state_[0] = min_;
-    state_[1] = min_;
-  }
+    // just clip desired values
+    // between 0 and 1000
+    Action apply_action(const Action &action_to_apply)
+    {
+        Action applied;
+        for (unsigned int i = 0; i < 2; i++)
+        {
+            if (action_to_apply.values[i] > max_)
+            {
+                applied.values[i] = max_;
+            }
+            else if (action_to_apply.values[i] < min_)
+            {
+                applied.values[i] = min_;
+            }
+            else
+            {
+                applied.values[i] = action_to_apply.values[i];
+            }
+            // simulating the time if could take for a real
+            // robot to perform the action
+            usleep(1000);
+            state_[i] = applied.values[i];
+        }
+        return applied;
+    }
 
-  // just clip desired values
-  // between 0 and 1000
-  Action apply_action(const Action &action_to_apply)
-  {
-    Action applied;
-    for(unsigned int i=0;i<2;i++)
-      {
-	if(action_to_apply.values[i]>max_)
-	  {
-	    applied.values[i]=max_;
-	  }
-	else if(action_to_apply.values[i]<min_)
-	  {
-	    applied.values[i]=min_;
-	  }
-	else
-	  {
-	    applied.values[i]=action_to_apply.values[i];
-	  }
-	// simulating the time if could take for a real
-	// robot to perform the action
-	usleep(1000);
-	state_[i] = applied.values[i];
-      }
-    return applied;
-  }
+    Observation get_latest_observation()
+    {
+        Observation observation;
+        observation.values[0] = state_[0];
+        observation.values[1] = state_[1];
+        return observation;
+    }
 
-  Observation get_latest_observation()
-  {
-    Observation observation;
-    observation.values[0] = state_[0];
-    observation.values[1] = state_[1];
-    return observation;
-  }
+    std::string get_error()
+    {
+        return "";  // no error
+    }
 
-  std::string get_error()
-  {
-      return ""; // no error
-  }
-    
-  void shutdown(){}
+    void shutdown()
+    {
+    }
 
 private:
-
-  int state_[2];
-  int min_;
-  int max_;
-
+    int state_[2];
+    int min_;
+    int max_;
 };
-
-
 
 int main()
 {
+    typedef robot_interfaces::RobotBackend<Action, Observation> Backend;
+    typedef robot_interfaces::RobotData<Action,
+                                        Observation,
+                                        robot_interfaces::Status>
+        Data;
+    typedef robot_interfaces::RobotFrontend<Action, Observation> Frontend;
 
-  typedef robot_interfaces::RobotBackend<Action,Observation> Backend;
-  typedef robot_interfaces::RobotData<Action,
-				      Observation,
-				      robot_interfaces::Status> Data;
-  typedef robot_interfaces::RobotFrontend<Action,Observation> Frontend;
-  
-  // max time allowed for the robot to apply an action.
-  double max_action_duration_s = 0.02;
-  
-  // max time allowed for 2 successive actions
-  double max_inter_action_duration_s = 0.05;
+    // max time allowed for the robot to apply an action.
+    double max_action_duration_s = 0.02;
 
-  
-  // demo showing the separated usage of backend and frontend
-  {
+    // max time allowed for 2 successive actions
+    double max_inter_action_duration_s = 0.05;
 
-    std::cout << "\n -- * -- Frontend and Backend -- * --\n" << std::endl;
-    
-    std::shared_ptr<Driver> driver_ptr = std::make_shared<Driver>(0,1000);
-    std::shared_ptr<Data> data_ptr = std::make_shared<Data>();
+    // demo showing the separated usage of backend and frontend
+    {
+        std::cout << "\n -- * -- Frontend and Backend -- * --\n" << std::endl;
 
-    Backend backend(driver_ptr,
-		    data_ptr,
-		    max_action_duration_s,
-		    max_inter_action_duration_s);
-    backend.initialize();
-  
-    Frontend frontend(data_ptr);
+        std::shared_ptr<Driver> driver_ptr = std::make_shared<Driver>(0, 1000);
+        std::shared_ptr<Data> data_ptr = std::make_shared<Data>();
 
-    Action action;
-    Observation observation;
+        Backend backend(driver_ptr,
+                        data_ptr,
+                        max_action_duration_s,
+                        max_inter_action_duration_s);
+        backend.initialize();
 
-    // simulated action :
-    // 1 dof going from 200 to 300
-    // The other going from 300 to 200
-  
-    for(uint value=200;value<=300;value++)
-      {
-	action.values[0]=value;
-	action.values[1]=500-value;
-	// this action will be stored at index
-	robot_interfaces::TimeIndex index = frontend.append_desired_action(action);
-	// getting the observation corresponding to the applied
-	// action, i.e. at the same index
-	observation = frontend.get_observation(index);
-	std::cout << "value: " << value << " | ";
-	action.print(false);
-	observation.print(true);
-      }
-  }
+        Frontend frontend(data_ptr);
 
-  // demo representing usage of frontend and backend
-  // encapsulated in the same instance
-  {
+        Action action;
+        Observation observation;
 
-    std::cout << "\n -- * -- Robot -- * --\n" << std::endl;
+        // simulated action :
+        // 1 dof going from 200 to 300
+        // The other going from 300 to 200
 
-    typedef robot_interfaces::Robot<Action,Observation,Driver> Robot;
+        for (uint value = 200; value <= 300; value++)
+        {
+            action.values[0] = value;
+            action.values[1] = 500 - value;
+            // this action will be stored at index
+            robot_interfaces::TimeIndex index =
+                frontend.append_desired_action(action);
+            // getting the observation corresponding to the applied
+            // action, i.e. at the same index
+            observation = frontend.get_observation(index);
+            std::cout << "value: " << value << " | ";
+            action.print(false);
+            observation.print(true);
+        }
+    }
 
-    int min = 0;
-    int max = 100;
-    Robot robot(max_action_duration_s,
-		max_inter_action_duration_s,
-		min,
-		max);
-    
-    robot.initialize();
+    // demo representing usage of frontend and backend
+    // encapsulated in the same instance
+    {
+        std::cout << "\n -- * -- Robot -- * --\n" << std::endl;
 
-    Action action;
-    Observation observation;
+        typedef robot_interfaces::Robot<Action, Observation, Driver> Robot;
 
-    // simulated action :
-    // 1 dof going from 200 to 300
-    // The other going from 300 to 200
-  
-    for(uint value=200;value<=300;value++)
-      {
-	action.values[0]=value;
-	action.values[1]=500-value;
-	// this action will be stored at index
-	robot_interfaces::TimeIndex index = robot.append_desired_action(action);
-	// getting the observation corresponding to the applied
-	// action, i.e. at the same index
-	observation = robot.get_observation(index);
-	std::cout << "value: " << value << " | ";
-	action.print(false);
-	observation.print(true);
-      }
-  }
+        int min = 0;
+        int max = 100;
+        Robot robot(
+            max_action_duration_s, max_inter_action_duration_s, min, max);
 
+        robot.initialize();
+
+        Action action;
+        Observation observation;
+
+        // simulated action :
+        // 1 dof going from 200 to 300
+        // The other going from 300 to 200
+
+        for (uint value = 200; value <= 300; value++)
+        {
+            action.values[0] = value;
+            action.values[1] = 500 - value;
+            // this action will be stored at index
+            robot_interfaces::TimeIndex index =
+                robot.append_desired_action(action);
+            // getting the observation corresponding to the applied
+            // action, i.e. at the same index
+            observation = robot.get_observation(index);
+            std::cout << "value: " << value << " | ";
+            action.print(false);
+            observation.print(true);
+        }
+    }
 }
--- include/robot_interfaces/robot.hpp
+++ include/robot_interfaces/robot.hpp
@@ -8,75 +8,68 @@
 
 #pragma once
 
-#include <robot_interfaces/robot_frontend.hpp>
 #include <robot_interfaces/robot_backend.hpp>
 #include <robot_interfaces/robot_data.hpp>
+#include <robot_interfaces/robot_frontend.hpp>
 #include <type_traits>
 
 namespace robot_interfaces
 {
-
 /**
  * @brief RobotFrontend that construct and encapsulates
  * its related RobotBackend. It also construct and starts
  * the robot driver.
  */
-template<typename Action,
-	 typename Observation,
-	 typename Driver>
-class Robot : public RobotFrontend<Action,Observation>
+template <typename Action, typename Observation, typename Driver>
+class Robot : public RobotFrontend<Action, Observation>
 {
+    typedef RobotData<Action, Observation, Status> Data;
+    typedef std::shared_ptr<Data> DataPtr;
+    typedef std::shared_ptr<Driver> RobotDriverPtr;
 
-  typedef RobotData<Action,Observation,Status> Data;
-  typedef std::shared_ptr<Data> DataPtr;
-  typedef std::shared_ptr<Driver> RobotDriverPtr;
-  
 public:
+    /**
+     * @param max_action_duration_s See MonitoredRobotDriver.
+     * @param max_inter_action_duration_s See MonitoredRobotDriver.
+     * @param args Arguments required to instantiate the driver
+     * by this constructor if not provided.
+     */
+    template <typename... Args>
+    Robot(double max_action_duration_s,
+          double max_inter_action_duration_s,
+          Args... args)
+        : RobotFrontend<Action, Observation>(std::make_shared<Data>()),
+          driver_ptr_(std::make_shared<Driver>(args...)),
+          backend_(driver_ptr_,
+                   this->robot_data_,
+                   max_action_duration_s,
+                   max_inter_action_duration_s)
+    {
+        // compile time checking template Driver inherate from RobotDriver
+        static_assert(
+            std::is_base_of<RobotDriver<Action, Observation>, Driver>::value,
+            "template Driver must be a subclass of "
+            "robot_interfaces::RobotDriver");
+    }
 
-  /**
-   * @param max_action_duration_s See MonitoredRobotDriver.
-   * @param max_inter_action_duration_s See MonitoredRobotDriver.
-   * @param args Arguments required to instantiate the driver
-   * by this constructor if not provided.
-   */
-  template<typename ... Args>
-  Robot(double max_action_duration_s,
-	double max_inter_action_duration_s,
-	Args ... args)
-    : RobotFrontend<Action,Observation>(std::make_shared<Data>()),
-	driver_ptr_(std::make_shared<Driver>(args ...)),
-	backend_(driver_ptr_,
-		 this->robot_data_,
-		 max_action_duration_s,
-		 max_inter_action_duration_s)
-  {
-    // compile time checking template Driver inherate from RobotDriver
-    static_assert(std::is_base_of<RobotDriver<Action,Observation>, Driver>::value,
-		  "template Driver must be a subclass of robot_interfaces::RobotDriver");
-  }
+    /**
+     * initialize the backend
+     */
+    void initialize()
+    {
+        backend_.initialize();
+    }
 
-  /**
-   * initialize the backend
-   */
-  void initialize()
-  {
-    backend_.initialize();
-  }
-        
-  /**
-   * return the data shared by the frontend and the backend.
-   */
-  const Data& get_data() const
-  {
-    return *(this->robot_data_);
-  }
+    /**
+     * return the data shared by the frontend and the backend.
+     */
+    const Data& get_data() const
+    {
+        return *(this->robot_data_);
+    }
 
-  
 private:
-
-  RobotDriverPtr driver_ptr_;
-  RobotBackend<Action,Observation> backend_;
-        
+    RobotDriverPtr driver_ptr_;
+    RobotBackend<Action, Observation> backend_;
 };
-    
 }
