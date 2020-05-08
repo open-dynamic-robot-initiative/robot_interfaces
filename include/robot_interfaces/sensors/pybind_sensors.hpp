dd6dfd609fcacde791ae5f20b8aa8b8910b6f9b5
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-05-08 08:50:29 +0200

    Implement multi-process sensor data

--- include/robot_interfaces/sensors/pybind_sensors.hpp
+++ include/robot_interfaces/sensors/pybind_sensors.hpp
@@ -25,9 +25,25 @@ namespace robot_interfaces
 template <typename ObservationType>
 void create_sensor_bindings(pybind11::module& m)
 {
-    pybind11::class_<SensorData<ObservationType>,
-                     std::shared_ptr<SensorData<ObservationType>>>(m, "Data")
-        .def(pybind11::init<>());
+    // some typedefs to keep code below shorter
+    typedef SensorData<ObservationType> BaseData;
+    typedef SingleProcessSensorData<ObservationType> SingleProcData;
+    typedef MultiProcessSensorData<ObservationType> MultiProcData;
+
+    pybind11::class_<BaseData, std::shared_ptr<BaseData>>(m, "BaseData");
+
+    pybind11::class_<SingleProcData,
+                     std::shared_ptr<SingleProcData>,
+                     BaseData>(m, "SingleProcessData")
+        .def(pybind11::init<size_t>(), pybind11::arg("history_size") = 1000);
+
+    pybind11::class_<MultiProcData,
+                     std::shared_ptr<MultiProcData>,
+                     BaseData>(m, "MultiProcessData")
+        .def(pybind11::init<std::string, bool, size_t>(),
+             pybind11::arg("shared_memory_id_prefix"),
+             pybind11::arg("is_master"),
+             pybind11::arg("history_size") = 1000);
 
     pybind11::class_<SensorDriver<ObservationType>,
                      std::shared_ptr<SensorDriver<ObservationType>>>(m,
