d01723e535de8f77caa53574b38af83bd7af9081
Felix Widmaier <luator@users.noreply.github.com>
2020-05-14 08:45:49 +0200

    Implement multi-process sensor data (#59)

--- include/robot_interfaces/sensors/sensor_data.hpp
+++ include/robot_interfaces/sensors/sensor_data.hpp
@@ -13,52 +13,81 @@
 #include <memory>
 #include <string>
 
+#include <time_series/multiprocess_time_series.hpp>
 #include <time_series/time_series.hpp>
 
 namespace robot_interfaces
 {
 /**
- * @brief Contains all the data coming from the sensors.
+ * @brief Contains the data coming from the sensors.
  *
- * @tparam ObservationType
+ * @tparam Observation  Type of the sensor observation.
  */
-template <typename ObservationType>
+template <typename Observation>
 class SensorData
 {
 public:
-    template <typename Type>
-    using Ptr = std::shared_ptr<Type>;
+    //! @brief Time series of the sensor observations.
+    std::shared_ptr<time_series::TimeSeriesInterface<Observation>> observation;
 
-    template <typename Type>
-    using Timeseries = time_series::TimeSeries<Type>;
+protected:
+    // make constructor protected to prevent instantiation of the base class
+    SensorData(){};
+};
 
-    SensorData(size_t history_length = 15000,
-               bool use_shared_memory = false,
-               // suppress unused warning (will be used in the future)
-               __attribute__((unused)) std::string shared_memory_address = "")
-    {
-        if (use_shared_memory)
-        {
-            std::cout << "shared memory sensor data is not implemented yet"
-                      << std::endl;
-            exit(-1);
-
-            // TODO: here we should check if the shared memory at that
-            // address already exists, otherwise we create it.
-            // we will also have to update timeseries such as to handle
-            // serialization internally (it will simply assume that the
-            // templated class has a method called serialize() and
-            // from_serialized())
-        }
-        else
-        {
-            observation =
-                std::make_shared<Timeseries<ObservationType>>(history_length);
-        }
-    }
-
+/**
+ * @brief SensorData instance using single process time series.
+ *
+ * Use this class if all modules accessing the data are running in the same
+ * process.  If modules run in separate processes, use MultiProcessSensorData
+ * instead.
+ *
+ * @copydoc SensorData
+ * @see MultiProcessSensorData
+ */
+template <typename Observation>
+class SingleProcessSensorData : public SensorData<Observation>
+{
 public:
-    Ptr<Timeseries<ObservationType>> observation;
+    SingleProcessSensorData(size_t history_length = 1000)
+    {
+        this->observation =
+            std::make_shared<time_series::TimeSeries<Observation>>(
+                history_length);
+    }
+};
+
+/**
+ * @brief SensorData instance using multi process time series.
+ *
+ * Use this class if modules accessing the data are running in separate
+ * processes.  When all modules run as threads in the same process, this class
+ * can be used as well, however, SingleProcessSensorData might be more efficient
+ * in that case.
+ *
+ * @copydoc SensorData
+ * @see SingleProcessSensorData
+ */
+template <typename Observation>
+class MultiProcessSensorData : public SensorData<Observation>
+{
+public:
+    MultiProcessSensorData(const std::string &shared_memory_id,
+                           bool is_master,
+                           size_t history_length = 1000)
+    {
+        if (is_master)
+        {
+            // the master instance is in charge of cleaning the memory
+            time_series::clear_memory(shared_memory_id);
+        }
+
+        const bool clean_on_destruction = is_master;
+
+        this->observation =
+            std::make_shared<time_series::MultiprocessTimeSeries<Observation>>(
+                shared_memory_id, history_length, clean_on_destruction);
+    }
 };
 
 }  // namespace robot_interfaces
